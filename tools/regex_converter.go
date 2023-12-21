665b4fd4d0f7dbc14cdd61f30049dd23f9659c27
MehmetErenTuranboy <mehmet.turanboy@gmail.com>
2023-12-21 23:00:06 +0300

    fix ? operation

--- tools/regex_converter.go
+++ tools/regex_converter.go
@@ -26,7 +26,7 @@ func ApplyPrecedence(c rune) int {
 }
 
 func OperatorLister(c rune) bool {
-	return strings.ContainsRune("|*.", c)
+	return strings.ContainsRune("?|*.", c)
 }
 
 func InfixToPostfix(infixRegex string) string {
@@ -69,7 +69,7 @@ type State struct {
 
 type nfa struct {
 	InitialState *State
-	endState     *State
+	EndState     *State
 }
 
 var StateID = 0
@@ -97,54 +97,68 @@ func AddConcatOperators(infix string) string {
 }
 
 func Compile(postfix string) *nfa {
+	fmt.Printf("\n Posfix of compilation : %s\n", postfix)
 	nfaStack := stack.New()
 
 	for _, c := range postfix {
 		switch c {
 		case '*':
+			fmt.Printf("* State")
 			peekNFA := nfaStack.Pop().(*nfa)
 			initial := NewState(EPSILON, peekNFA.InitialState, nil, false)
 			end := NewState(EPSILON, nil, nil, true)
 			initial.secondEdge = end
-			peekNFA.endState.firstEdge = initial
-			peekNFA.endState.secondEdge = end
+			peekNFA.EndState.firstEdge = initial
+			peekNFA.EndState.secondEdge = end
 			peekNFA.InitialState.isAccept = false
-			nfaStack.Push(&nfa{InitialState: initial, endState: end})
+			nfaStack.Push(&nfa{InitialState: initial, EndState: end})
 
 		case '.':
+			fmt.Printf(". State")
 			nfa2 := nfaStack.Pop().(*nfa)
 			nfa1 := nfaStack.Pop().(*nfa)
-			nfa1.endState.firstEdge = nfa2.InitialState
-			nfa1.endState.isAccept = false
-			nfaStack.Push(&nfa{InitialState: nfa1.InitialState, endState: nfa2.endState})
+			nfa1.EndState.firstEdge = nfa2.InitialState
+			nfa1.EndState.isAccept = false
+			nfaStack.Push(&nfa{InitialState: nfa1.InitialState, EndState: nfa2.EndState})
 
 		case '|':
 			fmt.Printf("| State")
 			nfa2 := nfaStack.Pop().(*nfa)
 			nfa1 := nfaStack.Pop().(*nfa)
+			fmt.Printf("\nNFA1 - Initial: %s, End: %s\n", string(nfa1.InitialState.label), string(nfa1.EndState.label))
+			fmt.Printf("NFA2 - Initial: %s, End: %s\n", string(nfa2.InitialState.label), string(nfa2.EndState.label))
 			initial := NewState(EPSILON, nfa1.InitialState, nfa2.InitialState, false)
 			end := NewState(EPSILON, nil, nil, true) // end State should be an accept State
-			nfa1.endState.firstEdge = end
-			nfa1.endState.isAccept = false // NFA1's end State is no longer an accept State
-			nfa2.endState.firstEdge = end
-			nfa2.endState.isAccept = false // NFA2's end State is no longer an accept State
-			nfaStack.Push(&nfa{InitialState: initial, endState: end})
+			nfa1.EndState.firstEdge = end
+			nfa1.EndState.isAccept = false // NFA1's end State is no longer an accept State
+			nfa2.EndState.firstEdge = end
+			nfa2.EndState.isAccept = false // NFA2's end State is no longer an accept State
+			nfaStack.Push(&nfa{InitialState: initial, EndState: end})
 
 		case '?':
-			peekNFA := nfaStack.Pop().(*nfa)
-			initial := NewState(EPSILON, peekNFA.InitialState, nil, false)
+			fmt.Printf("? State\n")
+			prevNFA := nfaStack.Pop().(*nfa)
+			initial := NewState(EPSILON, prevNFA.InitialState, nil, false)
 			end := NewState(EPSILON, nil, nil, true)
-			initial.secondEdge = end // Add an epsilon transition to the end state
-			peekNFA.endState.firstEdge = end
-			nfaStack.Push(&nfa{InitialState: initial, endState: end})
+
+			// Connect the end state of the previous NFA to the new end state
+			prevNFA.EndState.firstEdge = end
+
+			// Add an epsilon transition from the new initial state to the previous NFA's initial state
+			initial.firstEdge = prevNFA.InitialState
+
+			// Add another epsilon transition from the new initial state to the new end state
+			initial.secondEdge = end
+
+			// Push the new NFA fragment onto the stack
+			nfaStack.Push(&nfa{InitialState: initial, EndState: end})
 
 		default:
 			// Create two new States: One initial and one end State.
 			end := NewState(EPSILON, nil, nil, true) // This is the accept State.
 			initial := NewState(c, end, nil, false)  // The initial State transitions to the accept State on character c.
-
 			// Push the new NFA fragment onto the stack.
-			nfaStack.Push(&nfa{InitialState: initial, endState: end})
+			nfaStack.Push(&nfa{InitialState: initial, EndState: end})
 		}
 	}
 
